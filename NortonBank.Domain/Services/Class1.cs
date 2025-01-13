3e97e2cbd0afe78d68bde7dafae7305a835f4fb5
jaonorton <jaonorton@gmail.com>
2025-01-12 22:31:41 -0300

    Apenas criando o services

---
+++ NortonBank.Domain/Services/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NortonBank.Domain.Services        // aqui que ficam as regras de negócios
+{
+    internal class UserServices
+    {
+    }
+}
